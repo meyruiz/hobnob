2ce781ef3cb9cf77682db93eb5a2107514d8b0ea
Mayra Ruiz <mey.ruiz27@gmail.com>
2022-07-09 15:04:43 -0500

    Initial project setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import http from 'http';
+
+function requestHandler(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!');
+}
+const server = http.createServer(requestHandler);
+server.listen(8080);
