71c8c687b20c2be70df651ae6654bd7704a6c274
Mayra Ruiz <mey.ruiz27@gmail.com>
2022-07-24 17:30:39 -0500

    Create request in steps definition

--- spec/cucumber/steps/index.js
+++ spec/cucumber/steps/index.js
@@ -1,7 +1,12 @@
+import superagent from 'superagent';
 import { When, Then } from 'cucumber';
 
-When('the client creates a POST request to \\/users', function (callback) {
-  callback(null, 'pending');
+let request;
+let result;
+let error;
+
+When('the client creates a POST request to \\/users', function () {
+  request = superagent('POST', 'localhost:8080/users');
 });
 
 When('attaches a generic empty payload', function () {
@@ -20,8 +25,10 @@ When('sends the request', function (callback) {
     });
 });
 
-Then('our API should respond with a 400 HTTP status code', function (callback) {
-  callback(null, 'pending');
+Then('our API should respond with a 400 HTTP status code', function () {
+  if (error.statusCode !== 400) {
+    throw new Error();
+  }
 });
 
 Then('the payload of the response should be a JSON object', function (callback) {
